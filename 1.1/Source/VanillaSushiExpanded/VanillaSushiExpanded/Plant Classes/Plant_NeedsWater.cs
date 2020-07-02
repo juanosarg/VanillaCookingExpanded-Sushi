c868345d43fdc6a603b0e2d6559c0a2b9bec80e2
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-07-02 09:13:23 +0200

    v0.1

---
+++ 1.1/Source/VanillaSushiExpanded/VanillaSushiExpanded/Plant Classes/Plant_NeedsWater.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+
+using Verse;
+
+namespace VanillaSushiExpanded
+{
+    public class Plant_NeedsWater: Plant
+    {
+
+        const int radius = 6;
+        int numberOfWater = 0;
+
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            int num = GenRadial.NumCellsInRadius(radius);
+            for (int i = 0; i < num; i++)
+            {
+                IntVec3 c = this.Position + GenRadial.RadialPattern[i];
+                if (c.InBounds(map)) {
+                    TerrainDef terrain = c.GetTerrain(map);
+
+                    if (terrain != null && terrain.IsWater)
+                    {
+                        numberOfWater++;
+                    }
+
+                }
+                
+
+
+              
+            }
+
+        }
+
+        public override float GrowthRate
+        {
+            get
+            {
+                if (this.Blighted)
+                {
+                    return 0f;
+                }
+                if (base.Spawned && !PlantUtility.GrowthSeasonNow(base.Position, base.Map, false))
+                {
+                    return 0f;
+                }
+                return this.GrowthRateFactor_Fertility * this.GrowthRateFactor_Temperature * this.GrowthRateFactor_Light * this.GrowthRateFactor_Water;
+            }
+        }
+
+        public float GrowthRateFactor_Water
+        {
+            get
+            {
+               if (numberOfWater <= 20) {
+                    return 1f - ((20 - numberOfWater) * 0.05f);
+
+               }
+               else {
+                    return 1f;
+               }
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            
+            Scribe_Values.Look<int>(ref this.numberOfWater, "numberOfWater", 0, false);
+       
+        }
+
+
+    }
+}
